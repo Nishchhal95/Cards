5a226ebd59a51ba8db6f9f6da531b4b6282905ee
cshoaib05 <choudharyshoaib05@gmail.com>
2019-08-23 23:15:12 +0530

    player creater

---
+++ Assets/Scripts/PlayerCreater.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCreater : MonoBehaviour
+{
+    public List<Transform> placeholder;
+
+    int playercount;
+
+    public GameObject player;
+
+    private void Awake()
+    {
+        playercount = 0;
+    }
+
+
+    private void Start()
+    {
+        Placeplayer();
+    }
+
+
+    private void Placeplayer()
+    {
+        playercount = Random.Range(1, 6);
+        print(playercount);
+        for(int i =0; i<playercount;i++)
+        {
+            Instantiate(player,placeholder[i].transform.position,player.transform.rotation);
+        }
+    }
+}
