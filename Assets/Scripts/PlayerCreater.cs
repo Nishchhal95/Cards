1094a28213cda31e94e0f32e85c22f899e7bafae
cshoaib05 <choudharyshoaib05@gmail.com>
2019-08-24 17:32:44 +0530

    Players turn on key press

--- Assets/Scripts/PlayerCreater.cs
+++ Assets/Scripts/PlayerCreater.cs
@@ -5,13 +5,15 @@ public class PlayerCreater : MonoBehaviour
 {
     public List<Transform> placeholder;
     public List<Transform> place;
+    public List<GameObject> players;
 
     int playercount;
-
+    int indexInPlay;                                                                                               
     public GameObject player;
 
     private void Awake()
     {
+        indexInPlay = 0;
         playercount = 0;
     }
 
@@ -31,9 +33,59 @@ public class PlayerCreater : MonoBehaviour
         for(int i =0; i<playercount;i++)
         {
             int posindex = Random.Range(0, index--);
-            Instantiate(player,place[posindex].transform.position,player.transform.rotation);
+            GameObject go = Instantiate(player,place[posindex].transform.position,player.transform.rotation);
+            players.Add(go);
             place.RemoveAt(posindex);
         }
-        place = placeholder;
     }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            Blind();
+            Indexchange();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Show();
+            Indexchange();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Fold();
+            Indexchange();
+        }
+    }
+
+    void Blind()
+    {
+        print("Player " + indexInPlay + " Plays Blind");
+    }
+
+    void Show()
+    {
+        Debug.Log("Player " + indexInPlay + " Plays Show");
+    }
+
+    void Fold()
+    {
+        Debug.Log("Player " + indexInPlay + " Plays Fold");
+    }
+
+   void Indexchange()
+    {
+       if(indexInPlay==playercount)
+        {
+            indexInPlay=0;
+        }
+       else
+        {
+            indexInPlay++;
+        }
+    }
+
 }
+ 
