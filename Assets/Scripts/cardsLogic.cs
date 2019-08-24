e126e2736ce1ac06833d5ac3da21b7245dc51335
Smit <heysmitbarmase@gmail.com>
2019-08-24 19:50:07 +0530

    Card Class to work with Lists.

--- Assets/Scripts/cardsLogic.cs
+++ Assets/Scripts/cardsLogic.cs
@@ -5,89 +5,59 @@ using UnityEngine;
 
 
 public class cardsLogic : MonoBehaviour
-
-
-
-
 {
 
-
-
-    public List<string> cardDeck;// dynamic array to hold/ store  the cards 
-
-    // later it can be changed to list of objects
-
-
-    
-   public int playerNo;     // no of people to assign 3 cards to them.
-
+    public List<Card> CardDeck;  // List of Randomly Ordered 52 Cards.
+    public List<Card> DiscardDeck; // List of Cards which has been already used.
+    public int playerNo;     // no of people to assign 3 cards to them.
     int k;  // that will randomly fetch cards from database
 
-
+    public class Card   //A Card class we are going to use in Lists. A datatype.
+    {
+        public enum SuitEnum {  Hearts = 1, Clubs = 2, Diamonds = 3, Spades = 4 }
+        public int Rank;
+        public Card(SuitEnum suit, int rank)
+        {
+            //string assetName = string.Format("Card_{0}_{1}", suit, rank);  
+            // Example:  "Card_1_10" would be the Jack of Hearts.
+           // Debug.Log(assetName);
+        }
+    }
 
     void Start()
-
-
     {
         AssignCardToXpeople(); 
-
-
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-  
-
-    public void assignCArds()
-    {
-        // printing  3 random cards for a player
-
-        for (int i=0;i<3;i++)
-
-
-        {
-         
-
-            k = Random.Range(0, cardDeck.Count);  // get random index from list
-
-            Debug.Log(cardDeck[k]);    //fetching value from cards database
-
-
-            cardDeck.RemoveAt(k);  // avoid repeattion of aassignment of cards
-
-
-
-
-        }
-      
-      
-    }
-
-
 
     public void AssignCardToXpeople()
-
-
     {
-        Debug.Log("Assigning three random cards to" + playerNo + " number of players : " );
-
-
-
-        for(int i=1;i<=playerNo;i++)
-
+        Debug.Log("Assigning three random cards to" + playerNo + " number of players : ");
+        for (int i = 1; i <= playerNo; i++)
         {
-            Debug.Log("assigning 3 cards to player number  " + i ); // so that its known which card assigned to which player
-
-            assignCArds();
+            Debug.Log("assigning 3 cards to player number  " + i); // so that its known which card assigned to which player
+            AssignCards();
         }
-
-
-
-
-        
     }
+    public void AssignCards()
+    {   
+
+        //Here have to remove top card from deck.
+        //and add to discardedcard list
+        //and distribute this card to player.
+        //  WE HAVE TO CHANGE BELOW CODE.
+
+        // printing  3 random cards for a player
+        for (int i=0;i<3;i++)
+        {
+            k = Random.Range(0, CardDeck.Count);  // get random index from list
+
+            Debug.Log(CardDeck[k]);    //fetching value from cards database
+
+            CardDeck.RemoveAt(k);  // avoid repeattion of aassignment of cards
+        }
+    }
+
+
+
+ 
 }
