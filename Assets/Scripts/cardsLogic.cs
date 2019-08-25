9fc5b616c9ce6d48d5adaf9593e9317c39847693
surbhi shukla <surbhishukla38@gmail.com>
2019-08-25 18:18:49 +0530

    cardLogic

--- Assets/Scripts/cardsLogic.cs
+++ Assets/Scripts/cardsLogic.cs
@@ -9,8 +9,8 @@ public class cardsLogic : MonoBehaviour
    
 
 
-   public List<Card> CardDeck;  // List of Randomly Ordered 52 Cards.
-    public List<Card> DiscardDeck; // List of Cards which has been already used.
+  [SerializeField] public List<Card> CardDeck;  // List of Randomly Ordered 52 Cards.
+   // public List<Card> DiscardDeck; // List of Cards which has been already used.
     public int playerNo;     // no of people to assign 3 cards to them.
     int k;  // that will randomly fetch cards from database
 
@@ -56,11 +56,11 @@ public class cardsLogic : MonoBehaviour
                 Card CardToAdd = new Card((SuitEnum)i, j);      // "A" of Hearts.
                 CardDeck.Insert(0, CardToAdd); //Add it.
 
-               //  Debug.Log(CardToAdd.Rank + " of " + CardToAdd.Suit);
+              //  Debug.Log(CardToAdd.Rank + " of " + CardToAdd.Suit);
             }
         }
 
-        AssignCardToXpeople();
+       AssignCardToXpeople();
     }
 
 
@@ -86,9 +86,9 @@ public class cardsLogic : MonoBehaviour
 
     public void AssignCards()
     {  
-        //Assign Random Card to Player from List and Removes that Card from List.
+        //Assign 3 Random Card to Player from List and Removes that Card from List.
 
-        for (int i=0;i<3;i++)
+        for (int i=1;i<=3;i++)
         {
             k = Random.Range(0, CardDeck.Count);  // Get random index from cards list.
 
