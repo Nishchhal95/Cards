13d2de3e5d80d4973224a5b25a8f2c53f81c9773
cshoaib05 <choudharyshoaib05@gmail.com>
2019-08-25 16:36:53 +0530

    WebRequest

---
+++ Assets/Scripts/WebGetRequest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;                            
+
+public class WebGetRequest : MonoBehaviour
+{
+
+    void Start()
+    {
+        string url = "http://factoryprice.co.in/best_deals/testapi.php";
+        StartCoroutine(GetRequest(url));
+    }
+
+
+
+    IEnumerator GetRequest(string url)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+       
+            yield return webRequest.SendWebRequest();
+
+
+            if (webRequest.isNetworkError || webRequest.isHttpError)
+            {
+                Debug.Log(": Error: " + webRequest.error);
+            }
+            else
+            {
+                Debug.Log(":\nReceived: " + webRequest.downloadHandler.text);
+            }
+        }
+    }
+}
