8d70858e641a40046dbc18d2931cfa5971e880d4
Ritvik11 <36701232+Ritvik11@users.noreply.github.com>
2019-08-27 02:05:30 +0530

    Set MainPage & Shop UI Program

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Shop()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainPage");
+    }
+}
