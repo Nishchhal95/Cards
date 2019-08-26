91d52824dff9d5db51d1d472b60aa97d5a0c7b1f
Smit <heysmitbarmase@gmail.com>
2019-08-26 19:00:56 +0530

    Check Cards are in Order, Function To tell No. of Suits and Rank same.
    # Sort 3 cards in desending order.
    # Check Bool if Cards in Consecutive order or not.
    # How many INT Number of Cards are of Same Suit.
    # How many INT Number of Cards are of Same Rank.
    # Shows all this in Inspector.

--- Assets/Scripts/cardsLogic.cs
+++ Assets/Scripts/cardsLogic.cs
@@ -8,7 +8,7 @@ public class cardsLogic : MonoBehaviour
     // WARNING : DONT CHANGE THE ORDER IN WHICH SCRIPT LINES ARE WRITTEN. THEY MATTERS.
     //---------------------------------------------------------------------------------
     public enum SuitEnum { Hearts = 1, Clubs = 2, Diamonds = 3, Spades = 4 }
-    public enum ColorEnum { Red = 1, Black = 2}
+    public enum ColorEnum { Red = 1, Black = 2 }
 
     public List<Card> CardDeck = new List<Card>();  // List of Ordered 52 Cards.
 
@@ -17,7 +17,7 @@ public class cardsLogic : MonoBehaviour
 
     [System.Serializable]
     public class Card   //A Card class we are going to use in Lists. A datatype.
-    {     
+    {
         public SuitEnum Suit;
         public ColorEnum Color;
         public int Rank;
@@ -30,12 +30,17 @@ public class cardsLogic : MonoBehaviour
     }
 
     public List<CardList> PlayersList = new List<CardList>();  //List of Card of specific player.
-    
+
     [System.Serializable]
     public class CardList
     {
         public List<Card> CardsList = new List<Card>();  //List of Card of specific player.
         public int PlayerNo;
+
+        public bool Ordered;
+        public int NoOfSameSuit;
+        public int NoOfSameRank;
+
     }
 
     void Start()
@@ -47,11 +52,11 @@ public class cardsLogic : MonoBehaviour
     {
         //Create Ordered List of Cards.
 
-        for(int i=1; i<=4; i++)  //Loop for Suits
-        {    
-            for(int j=1; j<=13; j++)  //Loop for Number.
+        for (int i = 1; i <= 4; i++)  //Loop for Suits
+        {
+            for (int j = 1; j <= 13; j++)  //Loop for Number.
             {
-                if(i==1 || i== 3)  //For Color
+                if (i == 1 || i == 3)  //For Color
                 {
                     Card CardToAdd = new Card((SuitEnum)i, j, (ColorEnum)1);      // "A" of Hearts.
                     CardDeck.Insert(0, CardToAdd); //Add it.
@@ -75,23 +80,22 @@ public class cardsLogic : MonoBehaviour
             Debug.Log("Assigning 3 cards to Player Number " + i); // so that its known which card assigned to which player
             PlayersList.Add(new CardList() { PlayerNo = i });  //Add Player which Contains Cards and Player Number.
             AssignCards(i);
-            
+            CheckSequence(i);
+            CheckSuit(i);
+            CheckRank(i);
         }
     }
-
-
-
     public void AssignCards(int playerno)
     {
         //Assign Random Card to Player from List and Removes that Card from List.
 
-        for (int i=0;i<3;i++)
+        for (int i = 0; i < 3; i++)
         {
             k = Random.Range(0, CardDeck.Count);  // Get random index from cards list.
 
             Debug.Log(CardDeck[k].Rank + "of" + CardDeck[k].Suit);    // Fetching value from cards database.
 
-            PlayersList[playerno-1].CardsList.Add(new Card(CardDeck[k].Suit, CardDeck[k].Rank, CardDeck[k].Color));  //Add Card to respective Player.
+            PlayersList[playerno - 1].CardsList.Add(new Card(CardDeck[k].Suit, CardDeck[k].Rank, CardDeck[k].Color));  //Add Card to respective Player.
 
             CardDeck.RemoveAt(k);  // Avoid repeattion of aassignment of cards. 
 
@@ -99,12 +103,116 @@ public class cardsLogic : MonoBehaviour
 
     }
 
+    //---------------------------------
+
+
+    public void CheckRank(int ForWhichPlayer)  //Tells Behaviour of Deck with respect to "RANK".
+    {
+        if (PlayersList[ForWhichPlayer - 1].CardsList[0].Rank == PlayersList[ForWhichPlayer - 1].CardsList[1].Rank)
+        {
+            if (PlayersList[ForWhichPlayer - 1].CardsList[1].Rank == PlayersList[ForWhichPlayer - 1].CardsList[2].Rank)
+            {
+                //All have Same Rank.  // ALL 3 ARE SAME.
+                PlayersList[ForWhichPlayer - 1].NoOfSameRank = 3;
+            }
+            else
+            {
+                //Two Cards have Same Rank.  // FIRST 2 ARE SAME.  
+                PlayersList[ForWhichPlayer - 1].NoOfSameRank = 2;
+            }
+        }
+        else
+        {
+            if (PlayersList[ForWhichPlayer - 1].CardsList[1].Rank == PlayersList[ForWhichPlayer - 1].CardsList[2].Rank)
+            {
+                //Two Cards have Same Rank.   // LAST 2 ARE SAME.
+                PlayersList[ForWhichPlayer - 1].NoOfSameRank = 2;
+            }
+            else
+            {
+                //No Card have Same Rank.   // NONE ARE SAME.
+                PlayersList[ForWhichPlayer - 1].NoOfSameRank = 0;
+            }
+        }
+    }
+
+    public void CheckSuit(int ForWhichPlayer)  //Tells Behaviour of Deck with respect to "Suit".
+    {
+        if (PlayersList[ForWhichPlayer - 1].CardsList[0].Suit == PlayersList[ForWhichPlayer - 1].CardsList[1].Suit)
+        {
+            if (PlayersList[ForWhichPlayer - 1].CardsList[1].Suit == PlayersList[ForWhichPlayer - 1].CardsList[2].Suit)
+            {
+                //All have Same Suit.  // ALL 3 ARE SAME.
+                PlayersList[ForWhichPlayer - 1].NoOfSameSuit = 3;
+            }
+            else
+            {
+                //Two Cards have Same Suit.  // FIRST 2 ARE SAME.  
+                PlayersList[ForWhichPlayer - 1].NoOfSameSuit = 2;
+            }
+        }
+        else
+        {
+            if (PlayersList[ForWhichPlayer - 1].CardsList[1].Suit == PlayersList[ForWhichPlayer - 1].CardsList[2].Suit)
+            {
+                //Two Cards have Same Suit.   // LAST 2 ARE SAME.
+                PlayersList[ForWhichPlayer - 1].NoOfSameSuit = 2;
+            }
+            else
+            {
+                //No Card have Same Suit.   // NONE ARE SAME.
+                PlayersList[ForWhichPlayer - 1].NoOfSameSuit = 0;
+            }
+        }
+    }
+
+    public void SortInDesending(int ForWhichPlayer)
+    {
+        if (PlayersList[ForWhichPlayer - 1].CardsList[0].Rank <= PlayersList[ForWhichPlayer - 1].CardsList[1].Rank)
+        {
+            int temp = PlayersList[ForWhichPlayer - 1].CardsList[0].Rank;
+            PlayersList[ForWhichPlayer - 1].CardsList[0].Rank = PlayersList[ForWhichPlayer - 1].CardsList[1].Rank;
+            PlayersList[ForWhichPlayer - 1].CardsList[1].Rank = temp;
+        }
+
+        if (PlayersList[ForWhichPlayer - 1].CardsList[1].Rank <= PlayersList[ForWhichPlayer - 1].CardsList[2].Rank)
+        {
+            int temp = PlayersList[ForWhichPlayer - 1].CardsList[1].Rank;
+            PlayersList[ForWhichPlayer - 1].CardsList[1].Rank = PlayersList[ForWhichPlayer - 1].CardsList[2].Rank;
+            PlayersList[ForWhichPlayer - 1].CardsList[2].Rank = temp;
+        }
+
+        if (PlayersList[ForWhichPlayer - 1].CardsList[0].Rank <= PlayersList[ForWhichPlayer - 1].CardsList[1].Rank)
+        {
+            int temp = PlayersList[ForWhichPlayer - 1].CardsList[0].Rank;
+            PlayersList[ForWhichPlayer - 1].CardsList[0].Rank = PlayersList[ForWhichPlayer - 1].CardsList[1].Rank;
+            PlayersList[ForWhichPlayer - 1].CardsList[1].Rank = temp;
+        }
+    }
+
+    public void CheckSequence(int ForWhichPlayer) //Check Sequence of Cards in a Deck. Will return "True" if in sequence, else "False".
+    {
+        SortInDesending(ForWhichPlayer);  // NEED TO BE SORTED IN DESENDING ORDER.
+
+        if ((PlayersList[ForWhichPlayer - 1].CardsList[1].Rank == PlayersList[ForWhichPlayer - 1].CardsList[0].Rank - 1)
+            && (PlayersList[ForWhichPlayer - 1].CardsList[2].Rank == PlayersList[ForWhichPlayer - 1].CardsList[1].Rank - 1))
+        {
+            //IN CONSECUTIVE DESENDING SEQUENCE
+            PlayersList[ForWhichPlayer - 1].Ordered = true;
+        }
+        else
+        {
+            //NOT IN SEQUENCE
+            PlayersList[ForWhichPlayer - 1].Ordered = false;
+        }
+
+    }
 
     //---------------------------------
 
     // method to compare 3 ranks of the cards
 
-    public int compareHighestofThree(int a, int b, int c)
+    public void compareHighestofThree(int a, int b, int c)
     {
 
         int higheshPriority = a;
@@ -122,79 +230,20 @@ public class cardsLogic : MonoBehaviour
             higheshPriority = a;
         }
 
-        Debug.Log("card with highest priority is " + higheshPriority);
-        return higheshPriority;  // now method can also return the value
+        // Debug.Log("card with highest priority is " + higheshPriority);
     }
 
 
-    void sortingCardDeck()
-
-    {
-        CardDeck.Sort();
 
 
-    }
 
-
-    
 
     //to check rank of 3 cards index consecutively 
 
-
-
-
     void printHighestCardRank()
     {
-    
 
-        compareHighestofThree(CardDeck[0].Rank, CardDeck[1].Rank, CardDeck[2].Rank);
-
-       
-    }
-
-//-------------------------------------------------------
-
-    // finding all same suits, finding any two same suits, finding all diff suits
-
-    void methodForSuitCompare()
-
-
-    {
-
-        //permuation and combination logic for all 3 cards of same suit 
-        if(CardDeck[0].Suit==CardDeck[1].Suit    &&   CardDeck[0].Suit==CardDeck[2].Suit
-            
-            && CardDeck[1].Suit == CardDeck[2].Suit)
-
-
-
-        {
-            Debug.Log("all three suits are same");
-        }
-
-
-        //p & c logic for any 2 cards of same suit 
-        // out of 0 ,1,2
-        // 0 or 1 
-        // 0 02 1
-        //1 or 2
-
-
-      else   if   (CardDeck[0].Suit==CardDeck[1].Suit  || CardDeck[0].Suit == CardDeck[2].Suit || CardDeck[1].Suit == CardDeck[2].Suit)
-
-
-        {
-            Debug.Log("any two suits are same");
-        }
-
-
-
-
-        else
-
-        {
-            Debug.Log("all suits are of diffrent kind ");
-        }
+        compareHighestofThree(CardDeck[k].Rank, CardDeck[k + 1].Rank, CardDeck[k + 2].Rank);
     }
 
 
