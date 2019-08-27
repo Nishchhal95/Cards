173c5d689e53ebec72249abff8b7bacef3e889a6
surbhi shukla <surbhishukla38@gmail.com>
2019-08-27 14:05:27 +0530

    gameManger&Singleton

---
+++ Assets/gameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+
+
+    
+{
+
+
+    public static gameManager Instance { get; set; }
+    // Start is called before the first frame update
+    void Awake()
+    {
+    if (Instance == null)
+
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    else
+    {
+        Destroy(gameObject);
+    }
+
+    
+
+}
+
+    void Start()
+    {
+       
+
+        
+    }
+
+
+}
