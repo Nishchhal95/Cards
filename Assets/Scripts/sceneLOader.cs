f1e31cd44f4b6268834b0c2622ff55d58655e0db
surbhi shukla <surbhishukla38@gmail.com>
2019-08-29 17:08:11 +0530

    added srollcomponent , added sceneLoaderPrefaban& backBtn

---
+++ Assets/Scripts/sceneLOader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneLOader : MonoBehaviour
+{
+
+
+
+    [SerializeField]
+    private string nextSceneName = null;
+
+    /// <summary>
+    /// Ons the click transition button.
+    /// </summary>
+	public void OnClickTransitionButton()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+
+
+}
