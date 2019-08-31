26603ae67df3128e2d705d45d09b4d508f11b099
surbhi shukla <surbhishukla38@gmail.com>
2019-08-31 15:52:31 +0530

    how to play tab switch

---
+++ Assets/Scripts/tabSwicthingScrip.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tabSwicthingScrip : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+        public GameObject Basic;
+    public GameObject RAnking;
+
+    void Start()
+    {
+        Basic.SetActive(true);
+        RAnking.SetActive(false);
+    }
+    
+
+
+     public  void rankingClick()
+    {
+        Basic.SetActive(false);
+        RAnking.SetActive(true);
+    }
+
+
+    public void BasicClick()
+    {
+        RAnking.SetActive(false); // sequence of these two matters
+        Basic.SetActive(true);
+       
+    }
+
+
+
+
+}
