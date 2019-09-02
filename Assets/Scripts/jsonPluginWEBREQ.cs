195fea0ae4e6ec32afab1c04359dad3cc4800301
surbhi shukla <surbhishukla38@gmail.com>
2019-09-02 13:00:01 +0530

    webreq&jsonsoftPlugin

---
+++ Assets/Scripts/jsonPluginWEBREQ.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+
+public class jsonPluginWEBREQ : MonoBehaviour
+{
+
+    string jsonstring;
+
+
+    void Start()
+
+
+    {
+
+        StartCoroutine(GetText());
+
+
+    }
+
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://factoryprice.co.in/best_deals/getplayer.php");
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+
+        }
+        else
+        {
+
+            Debug.Log(www.downloadHandler.text);  // prints data from the url
+
+            jsonstring = www.downloadHandler.text;
+
+
+
+        
+
+            List<playerData> playerList = JsonConvert.DeserializeObject<List<playerData>>(jsonstring);
+
+
+            Debug.Log(playerList);
+
+        }
+
+
+
+
+    }
+
+
+    
+
+    public class playerData
+
+
+    {
+        public int coin;
+        public string name;
+        public string email;
+      
+
+    }
+
+
+
+
+
+}
