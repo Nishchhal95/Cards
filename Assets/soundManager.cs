b690bb2d20e9fb2bab5db403fbe6a989f0a227f8
surbhi shukla <surbhishukla38@gmail.com>
2019-09-03 06:34:35 +0530

    soundamanager for mute and unmute audio

---
+++ Assets/soundManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject soundcontroller;
+    void Start()
+    {
+        
+    }
+
+   public void SoundControll()
+    {
+        if(AudioListener.pause==true)
+        {
+            AudioListener.pause = false;
+        }
+
+        else
+        {
+            AudioListener.pause = true;
+        }
+    }
+}
