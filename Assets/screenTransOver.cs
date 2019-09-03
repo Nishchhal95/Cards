60a2aaed53ea3c9604256ae8b86153a5ca81c707
surbhi shukla <surbhishukla38@gmail.com>
2019-09-03 22:05:10 +0530

    fix canvasOverLap

---
+++ Assets/screenTransOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenTransOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject blackCanvas;
+
+    void Start()
+    {
+        blackCanvas.SetActive(true);
+    }
+
+
+    private void Update()
+    {
+        Destroy(blackCanvas, 2f);
+    }
+
+
+}
