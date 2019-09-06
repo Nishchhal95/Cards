690fa90b800acad645341a6d18b09278c1fd2c30
surbhi shukla <surbhishukla38@gmail.com>
2019-09-06 21:19:53 +0530

    webreqScript for sending any data

---
+++ Assets/SENDDATAwebReq.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SENDDATAwebReq : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(Upload());
+    }
+
+    IEnumerator Upload()
+    {
+        WWWForm form = new WWWForm();
+
+
+        form.AddField("myField", "myData");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://www.my-server.com/myform", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
