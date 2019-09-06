77419d99ce0f1cf429e78ca9fe3819c4666ee2af
surbhi shukla <surbhishukla38@gmail.com>
2019-09-06 22:41:30 +0530

    coindeduction webreq api

---
+++ Assets/coinDeductionAPI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class coinDeductionAPI : MonoBehaviour
+{
+
+    string userEmail= FB_Handler.instance.SavedEmail;
+
+    int coinsToBeDeducted=0;
+
+    string Deductedcoin;
+
+
+    void Start()
+    {
+        StartCoroutine(Upload());
+
+        Deductedcoin = coinsToBeDeducted.ToString();
+    }
+
+
+
+
+
+
+    IEnumerator Upload()
+    {
+        WWWForm form = new WWWForm();
+
+        
+
+        form.AddField("email of user ", userEmail);
+        form.AddField("Coins deducted ", Deductedcoin);
+
+
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://www.my-server.com/myform", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+
+}
