6cef347c75d31ef1aa187e1d85e72010676ecef9
surbhi shukla <surbhishukla38@gmail.com>
2019-09-06 21:51:15 +0530

    sending data to webrequest

--- Assets/SENDDATAwebReq.cs
+++ Assets/SENDDATAwebReq.cs
@@ -6,17 +6,42 @@ using UnityEngine.Networking;
 public class SENDDATAwebReq : MonoBehaviour
 {
 
+    string _name = FB_Handler.instance.SavedUsername;
+
+    string _email = FB_Handler.instance.SavedEmail;
+
+    Sprite _profile =FB_Handler.instance.SavedProfile;
+
+    
+
+
+
+
     void Start()
     {
         StartCoroutine(Upload());
+
+    
     }
+
+
+
+
+
 
     IEnumerator Upload()
     {
         WWWForm form = new WWWForm();
 
 
-        form.AddField("myField", "myData");
+
+        form.AddField("NameField", _name);
+
+        form.AddField("EmailField", _email);
+
+        form.AddField("ProfilePicture", _profile.ToString());
+
+        form.AddField("Coins", "5000 Bonus");
 
         using (UnityWebRequest www = UnityWebRequest.Post("http://www.my-server.com/myform", form))
         {
@@ -32,3 +57,4 @@ public class SENDDATAwebReq : MonoBehaviour
             }
         }
     }
+}
